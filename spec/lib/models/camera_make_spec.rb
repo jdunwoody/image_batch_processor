60c8b2572846b3466f71039706fb19d17d5d4790
James Dunwoody <jamesd@hooroo.com>
2014-03-02 14:18:10 +1100

    Changing tests for thumbnails.

--- spec/lib/models/camera_make_spec.rb
+++ spec/lib/models/camera_make_spec.rb
@@ -4,19 +4,34 @@ require './lib/models/camera_make'
 module Models
   describe CameraMake do
     describe '#add_model' do
+      let(:camera_make) { CameraMake.new('Camera Name') }
 
       it 'creates a camera model' do
-        camera_make = CameraMake.new('Camera Name')
         model = camera_make.add_model('Camera Make', 'thumbnail')
 
         expect(camera_make.models.size).to eq(1)
       end
 
       it 'returns the created camera model' do
-        camera_make = CameraMake.new('Camera Name')
         model = camera_make.add_model('Camera Make', 'thumbnail')
 
         expect(camera_make.models).to eq([model])
+      end
+
+      it 'populates thumbnails for this model' do
+        model = camera_make.add_model('Camera Make', 'thumbnail')
+
+        expect(model.thumbnails.urls.size).to eq(1)
+      end
+
+    end
+
+    describe '#name_matches?' do
+
+      it 'names match case insensitively, ignoring extra whitespace' do
+        camera_make = CameraMake.new('Camera Name')
+
+        expect(camera_make.name_matches?('  CaMeRa NaMe  ')).to be_true
       end
 
     end
